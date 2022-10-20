3ef941a143302bc3e1f2ba7c559f080e59b9372c
Alexander <arozinkov2@gmail.com>
2022-10-20 13:49:13 +0300

    not bad but u can better

--- yatube/posts/urls.py
+++ yatube/posts/urls.py
@@ -1,4 +1,4 @@
-# posts/urls.py
+﻿# posts/urls.py
 from django.urls import path
 
 from . import views
@@ -6,6 +6,6 @@ from . import views
 app_name = 'posts'
 
 urlpatterns = [
-	path('group/<slug:slug>/', views.group_posts, name = 'group_posts'),
+	path('group/<slug:slug>/', views.group_posts, name = 'group_list'),
     path('', views.index, name = 'main'),
 ] 
--- yatube/posts/views.py
+++ yatube/posts/views.py
@@ -1,20 +1,24 @@
-﻿from django.shortcuts import render
+﻿from django.shortcuts import render, get_object_or_404
 from django.http import HttpResponse
+from .models import Post, Group
 
 # Create your views here.
 def index(request):
     template = 'posts/index.html'
+    posts = Post.objects.order_by('-pub_date')[:10]
     context = {
-        'title': 'Блог о книшшках',
-        'text': 'Это главная страница проекта Yatube',
+        'posts': posts,
     }
     return render(request, template, context)
 	
 	
 def group_posts(request, slug):
     template = 'posts/group_posts.html'
+    group = get_object_or_404(Group, slug=slug)
+    posts = Post.objects.filter(group=group).order_by('-pub_date')[:10]
     context = {
-        'title': 'Группы',
-        'text': 'Здесь будет информация о группах проекта Yatube',
+        'group': group,
+        'posts': posts,
+		'titleGroup': group.title
     }
     return render(request, template, context) 	
